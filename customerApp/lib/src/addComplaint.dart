b0bf26110cc05241336613a7e01228e458b5d13c
abhijith <abhijith.v.m@lxisoft.com>
2020-07-14 20:03:21 +0530

    text box for register

--- customerApp/lib/src/addComplaint.dart
+++ customerApp/lib/src/addComplaint.dart
@@ -1,12 +1,65 @@
 import 'package:flutter/material.dart';
 
+class AddComplaint extends StatelessWidget {
+  static const String _title = ' Complaint Register ';
+  final _formKey = GlobalKey<FormState>();
 
-class AddComplaint extends StatelessWidget{
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Register your Complaint Here'),
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: MyStatefulWidget(),
+      ),
+    );
+
+  }
+}
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          TextFormField(
+            decoration: const InputDecoration(
+              hintText: 'tell us your problem',
+            ),
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter the issue';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: RaisedButton(
+              onPressed: () {
+                // Validate will return true if the form is valid, or false if
+                // the form is invalid.
+                if (_formKey.currentState.validate()) {
+                  // Process data.
+                }
+              },
+              child: Text('Submit'),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
