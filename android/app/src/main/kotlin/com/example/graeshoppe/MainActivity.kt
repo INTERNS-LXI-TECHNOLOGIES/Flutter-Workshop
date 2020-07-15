9aa8462f218a5640ef5728d8e6715181a0afe990
josemonjoseph <josemon.joseph@lxisoft.com>
2020-07-16 00:00:39 +0530

    all

--- android/app/src/main/kotlin/com/example/graeshoppe/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.graeshoppe
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:graeshoppe/page_1.dart';
-
-void main() {
-  runApp(MaterialApp(home: Page1()));
-}
