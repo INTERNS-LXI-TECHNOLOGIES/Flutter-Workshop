77fb6f83058c43328f43913e7cc0f744f85d33dd
greeshma01 <greeshma.c.g@lxisoft.com>
2020-07-17 00:10:56 +0530

    http://jira.bytatech.com:8080/browse/IN-8114

--- profileScreen/lib/main.dart
+++ profileScreen/lib/main.dart
@@ -20,16 +20,12 @@ class MyApp extends StatelessWidget {
         
  
        home: MyHomePage(title: 'Profile'),
-     
-        
-    
-     
-     
     );
-   
-    
-  }
+   }
+
 }
+
+
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -38,6 +34,10 @@ class MyHomePage extends StatefulWidget {
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
+
+   
+
+
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -46,30 +46,166 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        
       title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-            Icon(Icons.format_align_justify),
+            //  Image.asset("assets/download.jpg"),
+         
+            Icon(Icons.dehaze),
             Text(widget.title),
             Icon(Icons.power_settings_new)
           ],
         ),
 
       ),
-      body: Center(
-        child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Date 09/05/2019 12:43 PM'
+       body: Center(
+     child: Column(
+      children: <Widget>[
+      Row(children: <Widget>[
+         
+      Container(
+        
+        width: 200,
+        height: 200,
+        color: Colors.red,
+        
+      ),
+      Container(
+        
+       child: Text('test'
+       'test@gmail.com')
+        
+      )
+    ],
+    ), 
+
+   Row(
+    children: <Widget>[
+      Expanded(flex: 6,
+     child: Container(
+        
+        width: 700,
+        height: 40,
+        color: Colors.blue,
+        child: IconButton(icon:  Icon(Icons.dehaze), onPressed: null)
+      ),
+    ),
+    ],
+   ),
+
+
+    Row(
+       mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+      
+        Expanded(flex: 6,
+      child: Container(
+        
+        width: 200,
+        height: 50,
+       child: Text('FAVOURITES')
+        
+      ),
+      ),
+      Expanded(flex: 6,
+      child: Container(
+         width: 200,
+        height: 50,
+       child: Text('HISTORY')
+        
+      ),
+      ),
+    ],
+    ), 
+    Row(
+       mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+     
+        Expanded(flex: 6,
+          
+      child: Container(
+        
+        width: 200,
+        height: 50,
+       child: Text('FAVOURITES')
+        
+      ),
+      ),
+      Expanded(flex: 6,
+      child: Container(
+         width: 200,
+        height: 100,
+       child: Text('Date 09/05/2019 12:43 PM'
               'Grand Total 35.0'
               'Oder status payment-processed'
               'Store Name : four Star'
-              'Oder id : GR-1040',
-            ),
-            
-          ],
-        ),
+              'Oder id : GR-1040')
+        
       ),
+      ),
+    ],
+    ), 
+     Row(
+       mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+     
+        Expanded(flex: 6,
+          
+      child: Container(
+        
+        width: 200,
+        height: 50,
+       child: Text('FAVOURITES')
+        
+      ),
+      ),
+      Expanded(flex: 6,
+      child: Container(
+         width: 200,
+        height: 100,
+       child: Text('Date 09/05/2019 12:43 PM'
+              'Grand Total 35.0'
+              'Oder status payment-processed'
+              'Store Name : four Star'
+              'Oder id : GR-1040')
+        
+      ),
+      ),
+    ],
+    ), 
+     Row(
+       mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+     
+        Expanded(flex: 6,
+          
+      child: Container(
+        
+        width: 200,
+        height: 50,
+       child: Text('FAVOURITES')
+        
+      ),
+      ),
+      Expanded(flex: 6,
+      child: Container(
+         width: 200,
+        height: 100,
+       child: Text('Date 09/05/2019 12:43 PM'
+              'Grand Total 35.0'
+              'Oder status payment-processed'
+              'Store Name : four Star'
+              'Oder id : GR-1040')
+        
+      ),
+      ),
+    ],
+    ), 
+    ],
+    ),
+    ),
+        
       
 bottomNavigationBar: BottomNavigationBar( items: <BottomNavigationBarItem>[
             
@@ -78,15 +214,19 @@ bottomNavigationBar: BottomNavigationBar( items: <BottomNavigationBarItem>[
               title: Text('')
            ),
            BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarms),
+              icon: Icon(Icons.shopping_basket),
               title: Text('')
            ),
            BottomNavigationBarItem(
-              icon: Icon(Icons.access_alarms),
+              icon: Icon(Icons.person),
               title: Text('')
            )
            ],
-),
+           
+)
+      // appBar: AppBar(
+      //   title: Text('Flutter Flat Button Example'),
+      // ),
       
  );
  
