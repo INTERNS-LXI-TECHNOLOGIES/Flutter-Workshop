0b39eba6847afe3ba21daf655c3030463e1ef5df
ADDICTES <adarshpro1>
2021-04-15 14:29:24 +0530

    http://jira.bytatech.com:8080/browse/IN-14848

--- Car Project/Carproject.dart
+++ Car Project/Carproject.dart
@@ -2,12 +2,17 @@ import 'dart:io';
   void main()
 {
 	Car c = new Car();
-	c.aboutTheCar();
+	c.enterCarDetails();
 }
 class Car
 {
-	void carDetails()
+	void enterCarDetails()
 	{
+
+	print('                        CAR PROJECT                     ');
+    print('                     -----------------                  ');
+    print('\n');
+    print('\n');
 		
       print('ENTER THE DETAILS OF THE CAR AND DETAILS WILL PRINT AUTOMATICALY');
       print('-----------------------------------------------------------------');
@@ -16,75 +21,54 @@ class Car
 		String name=stdin.readLineSync();
 		print('Enter The Model Of Car : ');
 		String model=(stdin.readLineSync()); 
-      print('Enter The CompanyName of Car : ');
-      String companyname=stdin.readLineSync();
+        print('Enter The CompanyName of Car : ');
+        String companyname=stdin.readLineSync();
+
+        print('Enter The Engine Details : ');
+        print('Enter The Engine Number : ');
+        String number = stdin.readLineSync();
+        print('Enter The Model Name Of the Engine : ');
+        String modelname = stdin.readLineSync();
+  
+		print('Enter the Details of Doors : ');
+		print('Enter The Number OF Doors : ');
+		int doornumber=int.parse(stdin.readLineSync());
+		print('Enter The Type Of Door : ');
+		String door=stdin.readLineSync();
+
+		print('Enter the Details of Tyre : ');
+		print('Enter The Size OF Tyre : ');
+		String size=stdin.readLineSync();
+		print('Enter The Name Of Manufacture Of the Tyre : ');
+		String manufactname=stdin.readLineSync(); 
+
+		this.printCarDetails(name,model,companyname,number,modelname,doornumber,door,size,manufactname);
+	}
+
+	void printCarDetails(String name,String model,String companyname,String number,String modelname,int doornumber,String door,String size,String manufactname)
+	{
 		print('-------------------------------------');
 		print('Name of the CAR : ${name}');
 		print('CAR Model NAME: ${model}');
         print('CAR Company Name : ${companyname}');
         print('-------------------------------------');
-	}
 
-	void aboutTheCar()
-	{
-		print('                        CAR PROJECT                     ');
-    print('                     -----------------                  ');
-    print('\n');
-    print('\n');
-			this.carDetails();
-      print('\n');
-    print('\n');
-      this.engineDetails();
-      print('\n');
-    print('\n');
-      	this.doorDetails();
-        print('\n');
-    print('\n');
-        this.tyreDetails();
-        print('\n');
-    print('\n');
+        print('-------------------------------------');
+        print('Engine Details Of The Car  ');
+        print('Engine Number : ${number}');
+        print('Engine Model Name : ${modelname}');
+        print('-------------------------------------');
 
-	}
-  void engineDetails() 
-  {
-    print('Enter The Engine Details : ');
-    print('Enter The Engine Number : ');
-   String number = stdin.readLineSync();
-    print('Enter The Model Name Of the Engine : ');
-    String name = stdin.readLineSync();
-     print('-------------------------------------');
-    print('Engine Details Of The Car  ');
-    print('Engine Number : ${number}');
-    print('Engine Model Name : ${name}');
-     print('-------------------------------------');
-}
-  void doorDetails()
-	{
-		print('Enter the Details of Doors : ');
-
-		print('Enter The Number OF Doors : ');
-		int number=int.parse(stdin.readLineSync());
-		print('Enter The Type Of Door : ');
-		String door=stdin.readLineSync();
-     print('-------------------------------------');
-		print('Number of Doors : ${number}');
+        print('-------------------------------------');
+		print('Number of Doors : ${doornumber}');
 		print('Type of Door : ${door}');
-     print('-------------------------------------');
-	}
+        print('-------------------------------------');
 
-	void tyreDetails()
-	{
-		print('Enter the Details of Tyre : ');
 
-		print('Enter The Size OF Tyre : ');
-		String size=stdin.readLineSync();
-
-		print('Enter The Name Of Manufacture Of the Tyre : ');
-		String name=stdin.readLineSync();
-     print('-------------------------------------');
-		print('Tyre Manufactures name is : ${name}');
+        print('-------------------------------------');
+		print('Tyre Manufactures name is : ${manufactname}');
 		print('Tyre Size is : ${size}');
-     print('-------------------------------------');
+        print('-------------------------------------');
 
 	}
 }
