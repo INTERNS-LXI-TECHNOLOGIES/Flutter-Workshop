352ac9baff5ac5647273e40e986633609cf9c2f5
ADDICTES <adarshpro1>
2021-04-16 17:36:48 +0530

    http://jira.bytatech.com:8080/browse/IN-14885

--- Car Project/Tdd.dart
+++ Car Project/Tdd.dart
@@ -4,6 +4,16 @@ class Tdd
 void main()
 {
 	Car c = new Car();
-	c.enterandprintdetailsofCar();
+	Engine e = Car();
+	Door d = Car();
+	Tyre t = Car();
+	c.enterDetails();
+	c.printDetails();
+	e.enterDetails();
+	e.printDetails();
+	d.enterDetails();
+	d.printDetails();
+	t.enterDetails();
+	t.printDetails();
 }
 }
