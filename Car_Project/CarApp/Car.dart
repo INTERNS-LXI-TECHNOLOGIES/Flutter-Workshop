1f897723a085543c5c9f3d74edc95cbbb13d7685
faris-backer <faris.backer@lxisoft.com>
2021-04-16 17:55:50 +0530

    http://jira.bytatech.com:8080/browse/IN-14888

--- Car_Project/CarApp/Car.dart
+++ Car_Project/CarApp/Car.dart
@@ -10,26 +10,52 @@ class Car
     void printCarDetails()
     {
     	print('Details of the Given Car is Follows: ');
-		print('Name of the car : ${rc.carName}');
-		print('Model of the Car : ${rc.carModel}');
-        print('Manufacturer of the Car : ${rc.carManufacturer}');
+		print('Name of the car : 'rc.carName);
+		print('Model of the Car : 'rc.carModel);
+        print('Manufacturer of the Car : 'rc.carManufacturer);
 
         print('Details of the Given Car Registration certificate is Follows: ');
-		print('Name of RC Owner : ${rc.rcName}');
-		print('Adress of RC owner: ${rc.rcAdress}');
+		print('Name of RC Owner : 'rc.rcName);
+		print('Adress of RC owner: 'rc.rcAdress);
         
         print('Engine Details Are Following . ');
-    	print('Engine CC is : ${engine.engineCC}');
-    	print('Engine Type is : ${engine.engineType}');
+    	print('Engine CC is : 'engine.engineCC);
+    	print('Engine Type is : 'engine.engineType);
 
     	print('Details Of The Tyre :  ');
-		print('Tyre Manufactures name is :${tyre.tyreName}');
-		print('Tyre Size is : ${tyre.tyreSize}');
+		print('Tyre Manufactures name is : 'tyre.tyreName);
+		print('Tyre Size is : 'tyre.tyreSize);
 
 		print('Details Of The Door and Seating of vehicle : ');
-		print('Number of Doors : ${door.noofDoors}');
-		print('Type of Door : ${door.doorType}');
-		print('Number of seats Available : ${door.noofSeats}');
+		print('Number of Doors : 'door.noofDoors);
+		print('Type of Door : 'door.doorType);
+		print('Number of seats Available : 'door.noofSeats);
+    
+
+    }
+    void printCarDetails()
+    {
+    	print('Details of the Given Car is Follows: ');
+		print('Name of the car : 'rc.carName);
+		print('Model of the Car : 'rc.carModel);
+        print('Manufacturer of the Car : 'rc.carManufacturer);
+
+        print('Details of the Given Car Registration certificate is Follows: ');
+		print('Name of RC Owner : 'rc.rcName);
+		print('Adress of RC owner: 'rc.rcAdress);
+        
+        print('Engine Details Are Following . ');
+    	print('Engine CC is : 'engine.engineCC);
+    	print('Engine Type is : 'engine.engineType);
+
+    	print('Details Of The Tyre :  ');
+		print('Tyre Manufactures name is : 'tyre.tyreName);
+		print('Tyre Size is : 'tyre.tyreSize);
+
+		print('Details Of The Door and Seating of vehicle : ');
+		print('Number of Doors : 'door.noofDoors);
+		print('Type of Door : 'door.doorType);
+		print('Number of seats Available : 'door.noofSeats);
     
 
     }
