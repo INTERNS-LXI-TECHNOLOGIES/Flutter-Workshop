eb716026fdd810402933fcce8fcfb441d925be4b
faris-backer <faris.backer@lxisoft.com>
2021-04-27 12:58:35 +0530

    http://jira.bytatech.com:8080/browse/IN-15094

--- Car_Project/CarApp/Car.dart
+++ Car_Project/CarApp/Car.dart
@@ -3,32 +3,32 @@ class Car
      void printRCDetails(RC rc)
     {
     	print('Details of the Given Car is Follows: ');
-		print('Name of the car : ${rc.carName}');
-		print('Model of the Car : ${rc.carModel}');
-        print('Manufacturer of the Car : ${rc.carManufacturer}');
+		print('Name of the car : '+rc.carName+'');
+		print('Model of the Car : '+rc.carModel+'');
+        print('Manufacturer of the Car : '+rc.carManufacturer+'');
 
         print('Details of the Given Car Registration certificate is Follows: ');
-		print('Name of RC Owner : ${rc.rcName}');
-		print('Adress of RC owner: ${rc.rcAdress}');
+		print('Name of RC Owner : '+rc.rcName+'');
+		print('Adress of RC owner: '+rc.rcAdress+'');
     }
 
     void printEngineSpecifications(Engine engine)
     {        
     	print('Engine Details Are Following . ');
-    	print('Engine CC is : ${engine.engineCC}');
-    	print('Engine Type is : ${engine.engineType}');
+    	print('Engine CC is : '+engine.engineCC+'');
+    	print('Engine Type is : '+engine.engineType+'');
 	}
 	void printTyreDetails(Tyre tyre)
 	{
     	print('Details Of The Tyre :  ');
-		print('Tyre Manufactures name is : ${tyre.tyreName}');
-		print('Tyre Size is : ${tyre.tyreSize}');
+		print('Tyre Manufactures name is : '+tyre.tyreName+'');
+		print('Tyre Size is : '+tyre.tyreSize+'');
 	}
 	void printDoorAndSeatArrangements(Door door)
 	{
 		print('Details Of The Door and Seating of vehicle : ');
 		print('Number of Doors : ${door.noofDoors}');
-		print('Type of Door : ${door.doorType}');
+		print('Type of Door : '+door.doorType+'');
 		print('Number of seats Available : ${door.noofSeats}');
     
     }
