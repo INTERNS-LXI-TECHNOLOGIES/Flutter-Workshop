2de9f33cd9c4466a04ef823c0bc2bcaaa821aacc
faris-backer <faris.backer@lxisoft.com>
2021-08-17 19:24:11 +0530

    http://34.87.37.122:8080/browse/IN-17529

--- enewspaper/lib/CatagoryMainPage.dart
+++ enewspaper/lib/CatagoryMainPage.dart
@@ -47,88 +47,165 @@ class _CatagoryMainPageState extends State<CatagoryMainPage> {
           SizedBox(
             height: 20,
           ),
+          // Container(
+          //   height: height * .59,
+          //   width: double.infinity,
+          //   child: FutureBuilder<NewsDataCatagory>(
+          //       future: _newsDataCatagory,
+          //       builder: (context, snapshot) {
+          //         if (snapshot.hasData) {
+          //           // var keywordLength;S
+          //           // if(snapshot.data.results[index].keywords[index])
+          //           for (var index = 0;
+          //               index <= snapshot.data.results[index].keywords.length;
+          //               index++) {
+          //             var data = snapshot.data.results[index];
+          //             var title;
+          //             var imageurl;
+          //             var content;
+          //             var creator;
+          //             var pubDate;
+          //             if (data.keywords != null) {
+          //               if (data.keywords[index] == catagoryKeyword) {
+          //                 Container(
+          //                   child: Card(
+          //                     elevation: 5,
+          //                     child: ListTile(
+          //                       leading: Padding(
+          //                         padding: EdgeInsets.all(5),
+          //                         child: ClipRRect(
+          //                           borderRadius: BorderRadius.circular(10),
+          //                           child: Container(
+          //                             color: Colors.deepOrangeAccent,
+          //                             height: 50,
+          //                             width: 50,
+          //                           ),
+          //                         ),
+          //                       ),
+          //                       title: Text(data.title),
+          //                       subtitle: Text(data.description),
+          //                       onTap: () {
+          //                         title = data.title;
+          //                         content = data.description;
+          //                         creator = data.sourceId;
+          //                         pubDate = data.pubDate;
+          //                         Navigator.of(context).push(
+          //                           MaterialPageRoute(
+          //                             builder: (context) => NewsPage(title,
+          //                                 imageurl, content, creator, pubDate),
+          //                           ),
+          //                         );
+          //                       },
+          //                     ),
+          //                   ),
+          //                 );
+          //               } else {
+          //                 return Container();
+          //               }
+          //             } else {
+          //               return Column(
+          //                 children: [
+          //                   Text(
+          //                     'No Data Available on Server',
+          //                     style: TextStyle(
+          //                       fontFamily: 'Muli',
+          //                       fontWeight: FontWeight.bold,
+          //                       fontSize: 26,
+          //                       color: Colors.grey[800],
+          //                     ),
+          //                   ),
+          //                   SizedBox(
+          //                     height: 20,
+          //                   ),
+          //                   Image.asset(
+          //                     'assets/images/waiting.png',
+          //                     scale: 2.5,
+          //                   ),
+          //                 ],
+          //               );
+          //             }
+          //           }
+          //           return Center(child: CircularProgressIndicator());
+          //         } else {
+          //           throw Exception('${snapshot.error}');
+          //         }
+          //       }),
+          // ),
           Container(
             height: height * .59,
             width: double.infinity,
             child: FutureBuilder<NewsDataCatagory>(
-                future: _newsDataCatagory,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    for (var index = 0;
-                        index <= snapshot.data.results[index].keywords.length;
-                        index++) {
-                      var data = snapshot.data.results[index];
-                      var title;
-                      var imageurl;
-                      var content;
-                      var creator;
-                      var pubDate;
-                      if (data.keywords != null) {
-                        if (data.keywords[index] == catagoryKeyword) {
-                          Container(
-                            child: Card(
-                              elevation: 5,
-                              child: ListTile(
-                                leading: Padding(
-                                  padding: EdgeInsets.all(5),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Container(
-                                      color: Colors.deepOrangeAccent,
-                                      height: 50,
-                                      width: 50,
-                                    ),
-                                  ),
-                                ),
-                                title: Text(data.title),
-                                subtitle: Text(data.description),
-                                onTap: () {
-                                  title = data.title;
-                                  content = data.description;
-                                  creator = data.sourceId;
-                                  pubDate = data.pubDate;
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) => NewsPage(title,
-                                          imageurl, content, creator, pubDate),
-                                    ),
-                                  );
-                                },
+              future: _newsDataCatagory,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  for (int index = 0; index < 5; index++) {
+                    var data = snapshot.data.results[index];
+                    var title;
+                    var imageurl;
+                    var pubDate;
+                    var content;
+                    var creator;
+                    if (snapshot.data.results[index].keywords[index] != null) {
+                      Container(
+                        child: Card(
+                            child: ListTile(
+                          leading: Padding(
+                            padding: EdgeInsets.all(5),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Container(
+                                color: Colors.deepOrangeAccent,
+                                height: 50,
+                                width: 50,
                               ),
                             ),
-                          );
-                        } else {
-                          return Container();
-                        }
-                      } else {
-                        return Column(
-                          children: [
-                            Text(
-                              'No Data Available on Server',
-                              style: TextStyle(
-                                fontFamily: 'Muli',
-                                fontWeight: FontWeight.bold,
-                                fontSize: 26,
-                                color: Colors.grey[800],
+                          ),
+                          title: Text(data.title),
+                          subtitle: Text(data.description),
+                          onTap: () {
+                            title = data.title;
+                            content = data.description;
+                            creator = data.sourceId;
+                            pubDate = data.pubDate;
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => NewsPage(
+                                    title, imageurl, content, creator, pubDate),
                               ),
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Image.asset(
-                              'assets/images/waiting.png',
-                              scale: 2.5,
-                            ),
-                          ],
-                        );
-                      }
+                            );
+                          },
+                        )),
+                      );
                     }
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    throw Exception('${snapshot.error}');
                   }
-                }),
-          ),
+                } else {
+                  return Column(
+                    children: [
+                      Text(
+                        'No Data Available on Server',
+                        style: TextStyle(
+                          fontFamily: 'Muli',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 26,
+                          color: Colors.grey[800],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Image.asset(
+                        'assets/images/waiting.png',
+                        scale: 2.5,
+                      ),
+                    ],
+                  );
+                }
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
+              },
+            ),
+          )
         ],
       ),
     );
