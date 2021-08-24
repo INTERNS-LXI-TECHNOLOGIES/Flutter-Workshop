984d35263b2d102cb4fd75a3829f491ec9371377
faris-backer <faris.backer@lxisoft.com>
2021-08-24 10:12:17 +0530

    http://34.87.37.122:8080/browse/IN-17584

--- enewspaper/lib/apidata_manager.dart
+++ enewspaper/lib/apidata_manager.dart
@@ -112,4 +112,15 @@ class APIData_Manager {
       throw Exception('Failed to Load Data');
     }
   }
+
+  Future<NewsData> fetchSearchData(String searchText) async {
+    String url =
+        'https://newsdata.io/api/1/news?apikey=pub_6555c8e594aa52f948a54f505b72c2f473e&q=$searchText';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      return NewsData.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to Load Data');
+    }
+  }
 }
--- enewspaper/lib/home_screen.dart
+++ enewspaper/lib/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:enewspaper/catagoryMainPage.dart';
 import 'package:enewspaper/catagorypage.dart';
 import 'package:enewspaper/news_page.dart';
 import 'package:enewspaper/newsdata.dart';
+import 'package:enewspaper/searchPage.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -23,58 +24,108 @@ class _HomeScreenState extends State {
     super.initState();
   }
 
+  final _searchInputController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: SafeArea(
-          child: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: height * .04,
-            ),
-            headerBox(height, width),
-            SizedBox(height: 20),
-            hotNewstitleSection(),
-            SizedBox(height: 10),
-            Container(
-              height: height * .2,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    SizedBox(
-                      width: 20,
-                    ),
-                    catagorySection(context, 'Sports',
-                        'assets/images/Sports.jpg', 'Sports', ''),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    catagorySection(context, 'Entertainment',
-                        'assets/images/Entertainment.jpg', 'Entertainment', ''),
-                    SizedBox(
-                      width: 20,
-                    ),
-                    catagorySection(context, 'Business',
-                        'assets/images/business.jpg', 'Business', ''),
-                    SizedBox(
-                      width: 20,
-                    ),
-                  ],
+      body: GestureDetector(
+        onTap: () {
+          FocusScopeNode currentFocus = FocusScope.of(context);
+
+          if (!currentFocus.hasPrimaryFocus) {
+            currentFocus.unfocus();
+          }
+        },
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: height * .04,
+              ),
+              Row(
+                children: [
+                  headerBox(height, width),
+                  SizedBox(width: 10),
+                  searchBar(width, context)
+                ],
+              ),
+              SizedBox(height: 20),
+              hotNewstitleSection(),
+              SizedBox(height: 10),
+              Container(
+                height: height * .2,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      SizedBox(
+                        width: 20,
+                      ),
+                      catagorySection(context, 'sports',
+                          'assets/images/Sports.jpg', 'Sports', ''),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      catagorySection(
+                          context,
+                          'entertainment',
+                          'assets/images/Entertainment.jpg',
+                          'Entertainment',
+                          ''),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      catagorySection(context, 'business',
+                          'assets/images/business.jpg', 'Business', ''),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            newsViewerSection(height, width),
-          ],
+              SizedBox(
+                height: 20,
+              ),
+              newsViewerSection(height, width),
+            ],
+          ),
+        )),
+      ),
+    );
+  }
+
+  Container searchBar(double width, BuildContext context) {
+    return Container(
+      width: width * .6,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: InputBorder.none,
+          focusedBorder: InputBorder.none,
+          hintText: "Search News",
+          prefixIcon: Icon(Icons.search),
+          contentPadding: EdgeInsets.symmetric(vertical: 9, horizontal: 20),
         ),
-      )),
+        controller: _searchInputController,
+        keyboardType: TextInputType.text,
+        onSubmitted: (value) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => SearchPage(_searchInputController.text),
+            ),
+          );
+        },
+        textInputAction: TextInputAction.go,
+      ),
     );
   }
 
@@ -226,17 +277,16 @@ class _HomeScreenState extends State {
         children: [
           Container(
             margin: EdgeInsets.only(left: 20),
-            height: height * 0.16,
-            width: width * .9,
+            height: height * 0.15,
+            width: width * .3,
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.deepOrangeAccent),
-          ),
-          Container(
-            height: height * .16,
-            alignment: Alignment.center,
-            child: Image.network(
-                'https://www.freepnglogos.com/uploads/fox-news-png-logo/news-live-png-logo-27.png'),
+              borderRadius: BorderRadius.circular(20),
+              // color: Colors.deepOrangeAccent,
+            ),
+            child: Image.asset(
+              'assets/images/enews_logo.png',
+              fit: BoxFit.scaleDown,
+            ),
           ),
         ],
       ),
